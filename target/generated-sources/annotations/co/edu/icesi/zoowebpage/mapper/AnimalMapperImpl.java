5b8ff9824ea90888abe6ef049ce878bc94711d7e
Kennet Santiago Sánchez Roldán <78995084+KennetSanchez@users.noreply.github.com>
2022-09-24 20:32:20 -0500

    AnimalWithParentsDTO added, post working get ain't
    * The post methods are working fine
    * There's a bug with the get by name and get by id method, the mapping it's has an ambiguous handler

--- target/generated-sources/annotations/co/edu/icesi/zoowebpage/mapper/AnimalMapperImpl.java
+++ target/generated-sources/annotations/co/edu/icesi/zoowebpage/mapper/AnimalMapperImpl.java
@@ -1,20 +1,59 @@
 package co.edu.icesi.zoowebpage.mapper;
 
 import co.edu.icesi.zoowebpage.dto.AnimalDTO;
+import co.edu.icesi.zoowebpage.dto.AnimalWithParentsDTO;
 import co.edu.icesi.zoowebpage.model.Animal;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-09-24T10:23:46-0500",
-    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
+    date = "2022-09-24T20:13:39-0500",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 18.0.2 (Oracle Corporation)"
 )
 @Component
 public class AnimalMapperImpl implements AnimalMapper {
 
     @Override
     public Animal fromDTO(AnimalDTO animalDTO) {
+        if ( animalDTO == null ) {
+            return null;
+        }
+
+        Animal.AnimalBuilder animal = Animal.builder();
+
+        animal.id( animalDTO.getId() );
+        animal.name( animalDTO.getName() );
+        animal.sex( animalDTO.getSex() );
+        animal.weight( animalDTO.getWeight() );
+        animal.age( animalDTO.getAge() );
+        animal.height( animalDTO.getHeight() );
+        animal.arrivalDate( animalDTO.getArrivalDate() );
+
+        return animal.build();
+    }
+
+    @Override
+    public AnimalDTO fromAnimal(Animal animal) {
+        if ( animal == null ) {
+            return null;
+        }
+
+        AnimalDTO animalDTO = new AnimalDTO();
+
+        animalDTO.setId( animal.getId() );
+        animalDTO.setName( animal.getName() );
+        animalDTO.setSex( animal.getSex() );
+        animalDTO.setWeight( animal.getWeight() );
+        animalDTO.setAge( animal.getAge() );
+        animalDTO.setHeight( animal.getHeight() );
+        animalDTO.setArrivalDate( animal.getArrivalDate() );
+
+        return animalDTO;
+    }
+
+    @Override
+    public Animal fromDTOWithParents(AnimalWithParentsDTO animalDTO) {
         if ( animalDTO == null ) {
             return null;
         }
@@ -35,23 +74,23 @@ public class AnimalMapperImpl implements AnimalMapper {
     }
 
     @Override
-    public AnimalDTO fromAnimal(Animal animal) {
+    public AnimalWithParentsDTO fromAnimalWithParents(Animal animal) {
         if ( animal == null ) {
             return null;
         }
 
-        AnimalDTO animalDTO = new AnimalDTO();
+        AnimalWithParentsDTO animalWithParentsDTO = new AnimalWithParentsDTO();
 
-        animalDTO.setId( animal.getId() );
-        animalDTO.setFatherId( animal.getFatherId() );
-        animalDTO.setMotherId( animal.getMotherId() );
-        animalDTO.setName( animal.getName() );
-        animalDTO.setSex( animal.getSex() );
-        animalDTO.setWeight( animal.getWeight() );
-        animalDTO.setAge( animal.getAge() );
-        animalDTO.setHeight( animal.getHeight() );
-        animalDTO.setArrivalDate( animal.getArrivalDate() );
+        animalWithParentsDTO.setId( animal.getId() );
+        animalWithParentsDTO.setFatherId( animal.getFatherId() );
+        animalWithParentsDTO.setMotherId( animal.getMotherId() );
+        animalWithParentsDTO.setName( animal.getName() );
+        animalWithParentsDTO.setSex( animal.getSex() );
+        animalWithParentsDTO.setWeight( animal.getWeight() );
+        animalWithParentsDTO.setAge( animal.getAge() );
+        animalWithParentsDTO.setHeight( animal.getHeight() );
+        animalWithParentsDTO.setArrivalDate( animal.getArrivalDate() );
 
-        return animalDTO;
+        return animalWithParentsDTO;
     }
 }
